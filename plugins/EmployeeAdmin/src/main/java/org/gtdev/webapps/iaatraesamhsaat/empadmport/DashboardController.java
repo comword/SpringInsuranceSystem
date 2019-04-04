c49acb34157cf44afae361013551efaf5f1a6888
Teffy <2528377329@qq.com>
2019-04-04 20:01:53 +0800

    [EmployeeDashboard] The navigation bar has been rewritten to suit mobile browsers. Write 3 HTML files to show the information of claim details, product information and customer information.

--- plugins/EmployeeAdmin/src/main/java/org/gtdev/webapps/iaatraesamhsaat/empadmport/DashboardController.java
+++ plugins/EmployeeAdmin/src/main/java/org/gtdev/webapps/iaatraesamhsaat/empadmport/DashboardController.java
@@ -1,5 +1,7 @@
 package org.gtdev.webapps.iaatraesamhsaat.empadmport;
 
+import org.pf4j.Extension;
+import org.pf4j.ExtensionPoint;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,10 +10,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.LocaleResolver;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.Locale;
 
+@Extension
 @Controller
-public class DashboardController {
-    private Logger Log = LoggerFactory.getLogger(this.getClass());
+public class DashboardController implements ExtensionPoint {
+    private Logger Log = LoggerFactory.getLogger(DashboardController.class);
 
     @Autowired
     private LocaleResolver localeResolver;
@@ -24,4 +28,44 @@ public class DashboardController {
 //        else
             return "employee/admin/admin";
     }
+
+
+    @GetMapping("/employee/admin/detail")
+    public String detail(HttpServletRequest request) {
+//        Locale l = localeResolver.resolveLocale(request);
+//        if(l.equals(Locale.SIMPLIFIED_CHINESE))
+//            return "employee/admin/admin";
+//        else
+        return "employee/admin/detail";
+    }
+
+    @GetMapping("/employee/admin/product")
+    public String product(HttpServletRequest request) {
+//        Locale l = localeResolver.resolveLocale(request);
+//        if(l.equals(Locale.SIMPLIFIED_CHINESE))
+//            return "employee/admin/admin";
+//        else
+        return "employee/admin/product";
+    }
+
+    @GetMapping("/employee/admin/customer")
+    public String customer(HttpServletRequest request) {
+//        Locale l = localeResolver.resolveLocale(request);
+//        if(l.equals(Locale.SIMPLIFIED_CHINESE))
+//            return "employee/admin/admin";
+//        else
+        return "employee/admin/customer";
+    }
+
+    @GetMapping("/employee/login/login")
+    public String sign(HttpServletRequest request) {
+//        Locale l = localeResolver.resolveLocale(request);
+//        if(l.equals(Locale.SIMPLIFIED_CHINESE))
+//            return "employee/admin/admin";
+//        else
+        return "employee/login/login";
+    }
+
+
+
 }
